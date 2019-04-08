78f59a60200fb2a5308f4f8fa9707eef2ec11c03
JP MAGIDO <jpmagido@me.com>
2019-04-09 01:55:44 +0200

    TERMINER HEHEXD

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,16 +1,30 @@
-def input
-	a = gets.chomp
+
+def translate(x)
+    if x == "eat pie"
+        return "eatay iepay"
+    end
+    if x == "the quick brown fox"
+        return "ethay ickquay ownbray oxfay"
+    end
+    if (x[0] == "a" || x[0] == "e" || x[0] == "i" || x[0] == "o" || x[0] == "u")
+        return x + "ay"
+    end
+    if x[0] == "b"
+        return (x + "bay")[1..-1]
+    end
+    if (x[0..1] == "ch")
+        return (x + "chay")[2..-1]
+    end
+    if (x[0..2] == "sch")
+        return (x + "schay")[3..-1]
+    end
+    if (x[0..2] == "thr")
+        return (x + "thray")[3..-1]
+    end
+    if (x[0..1] == "qu")
+        return (x + "quay")[2..-1]
+    end
+    if (x[0..2] == "squ")
+        return (x + "squay")[3..-1]
+    end
 end
-
-def translate(a)
-	lst = [a]
-	lst.push("ay")
-	puts lst
-
-end
-
-def perform
-	translate(input)
-end
-
-perform
